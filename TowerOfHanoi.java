ade67f841765ef4453fa490ec95b048c25410658
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 18:49:11 +0530

    Update TowerOfHanoi.java

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,1 +1,36 @@
+class Hanoi {
 
+    public long toh(int N, int from, int to, int aux) {
+        // Your code here
+        if(N==1){
+            System.out.println("move disk "+1+" from rod "+from+" to rod "+to);
+            return 1;
+        }
+        
+        long a = toh(N-1,from,aux,to);
+        System.out.println("move disk "+N+" from rod "+from+" to rod "+to);
+        long b = toh(N-1,aux,to,from);
+        
+        return 1+a+b;
+        
+        
+    }
+}
+class Hanoi {
+
+    public long toh(int N, int from, int to, int aux) {
+        // Your code here
+        if(N==1){
+            System.out.println("move disk "+1+" from rod "+from+" to rod "+to);
+            return 1;
+        }
+        
+        long a = toh(N-1,from,aux,to);
+        System.out.println("move disk "+N+" from rod "+from+" to rod "+to);
+        long b = toh(N-1,aux,to,from);
+        
+        return 1+a+b;
+        
+        
+    }
+}
