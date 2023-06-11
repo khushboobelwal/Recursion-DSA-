4667866c1fc8d1115decf7189c35ae84e850ea32
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 18:32:41 +0530

    Update Print N to 1.java

--- Print N to 1.java
+++ Print N to 1.java
@@ -1,1 +1,18 @@
+class Solution {
+    
 
+    static void printNos(int N) {
+        if (N <= 0)
+    {
+        return;
+    }
+    else
+    {
+        System.out.print(N + " ");
+ 
+        // Recursive call of the function
+        printNos(N - 1);
+    }
+}
+        // code here
+}  
