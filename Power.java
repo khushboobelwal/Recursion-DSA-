c487ab4a22701e16964b4f12eadae25c51b7cdc6
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 17:28:12 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -1,1 +1,20 @@
+public class Power {
+	static int pow(int number,int power ) {
+		if(power==0) {
+			return 1;
+		}
+		else 
+		{
+			return number*pow(number,power-1);
+		}
+	}
 
+	public static void main(String[] args) {
+		int number=2;
+		int power=4;
+		System.out.println(pow(number,power));
+		// TODO Auto-generated method stub
+
+	}
+
+}
