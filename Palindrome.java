eada7dd7dbc8bdc676ed12efbce6fafe5b3bd68f
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 18:17:54 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    int Palindrome(String S) {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Character> second = new ArrayList<>();
+        for(int i=S.length()-1; i>=0; i--){
+            second.add(S.charAt(i));
+        }
+        int j=0;
+        for(Character s: second){
+            sb.append(s);
+        }
+        if(S.contains(sb.toString())){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+};
