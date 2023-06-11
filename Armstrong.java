28c2b609c842a99a2aa675335f4238c89d173220
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 17:41:27 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,1 +1,33 @@
+public class Armstrong {
+	static int rev=0;
+	static int r;
+	
+	public static int reverse(int number) {
+		
+		
+		if(number>0) {
+	    r=number%10;
+	    rev=rev+r*r*r;
+		reverse(number/10);
+	}	
+		return rev;	
+	}
+	
+
+	public static void main(String args[]) {
+		int number=153;
+		reverse(number);
+		
+		if(rev==number) {
+			System.out.println("Armstrong");
+			
+		}
+		else {
+			System.out.println("Not Armstrong");
+		}
+	}
+	}
+	
+	
+
 
