d4ebc683fd9689bf820ffb2e56756468a897913d
khushboobelwal <106762313+khushboobelwal@users.noreply.github.com>
2023-06-11 17:25:06 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,1 +1,23 @@
+class MissingNumber {
+    public static int MissingNumber(int[] nums) {
+        int sum=0;
+        int expectedSum=0;
+        int len=nums.length;
+        for(int i=0;i<=len;i++){
+            expectedSum+=i;
+        }
+        for(int j=0;j<len;j++){
+            sum+=nums[j];
+        }
+        int missing=expectedSum-sum;
+        return missing;
+        
+    }
 
+    public static void main(String[] args) {
+       int nums[]={3,0,1};
+       System.out.println(MissingNumber(nums));
+    }
+    }
+
+	
